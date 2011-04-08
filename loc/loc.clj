f825f812fe62eed2128a3eb4b5bc67d232bb8ea0
kitofr <kitofr@gmail.com>
2011-04-08 14:34:50 +0200

    Rename :/

--- loc/loc.clj
+++ loc/loc.clj
@@ -2,12 +2,12 @@
 
 (defn non-blank? [line] (if (re-find #"\S" line) true false))
 (defn non-svn? [file] (not (.contains (.toString file) ".svn")))
-(defn cs-source? [file] (.endsWith (.toString file) ".cs"))
+(defn cs-source? [file] (and (not (.contains (.toString file) "designer")) (.endsWith (.toString file) ".cs")))
 (defn cs-loc [base-file]
   (reduce
     +
     (for [file (file-seq base-file)
           :when (and (cs-source? file) (non-svn? file))]
-      (with-open [rdr (read-file)
+      (with-open [rdr (reader file)]
                   (count (filter non-blank? (line-seq rdr)))))))
 
