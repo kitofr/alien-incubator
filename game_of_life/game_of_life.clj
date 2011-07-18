39b88ee56804d950fca2fa4cdb88842c093c334f
kitofr <kitofr@gmail.com>
2011-07-18 21:24:24 +0200

    I think animals are immutable... so need to return a copy

--- game_of_life/game_of_life.clj
+++ game_of_life/game_of_life.clj
@@ -6,7 +6,7 @@
 
 (defn random-plant [left top width height]
   (let [pos (cons (+ left (rand-int width)) (list (+ top (rand-int height))))]
-        (conj plants (.hashCode pos))))
+    (conj plants (.hashCode pos))))
 
 (defn add-plants []
   (apply #'random-plant jungle)
@@ -16,12 +16,12 @@
 
 (def animals
   (list (struct-map animal 
-                    :x 50;;(/ 2 width)
-                    :y 15;;(/ 2 height)
+                    :x (int (/ width 2))
+                    :y (int (/ height 2))
                     :energy 1000
                     :dir 0
                     :genes (take 8
-                             (repeatedly #(rand-int 10))))))
+                                 (repeatedly #(rand-int 10))))))
 
 (defn move [animal]
   (let [dir (:dir animal)
@@ -29,11 +29,11 @@
         y (:y animal)]
     (assoc animal :x (mod (+ x
                              (cond 
-                                (and (>= dir 2) (< dir 5)) 1
-                                (or (= dir 1) (= dir 5)) 0
-                                :else -1)
-                                   width)
-                             width))
+                               (and (>= dir 2) (< dir 5)) 1
+                               (or (= dir 1) (= dir 5)) 0
+                               :else -1)
+                             width)
+                          width))
     (assoc animal :y (mod (+ y
                              (cond (and (>= dir 0) (< dir 3)) -1
                                    (and (>= dir 4) (< dir 7)) 1
@@ -42,23 +42,3 @@
                           height))
     (dec (:energy animal))))
 
-      
-                                      
-;;(defun move (animal)
-;;  (let ((dir (animal-dir animal))
-;;        (x (animal-x animal))
-;;        (y (animal-y animal)))
-;;    (setf (animal-x animal) (mod (+ x
-;;                                    (cond ((and (>= dir 2) (< dir 5)) 1)
-;;                                          ((or (= dir 1) (= dir 5)) 0)
-;;                                          (t -1))
-;;                                    *width*)
-;;                                 *width*))
-;;    (setf (animal-y animal) (mod (+ y
-;;                                    (cond ((and (>= dir 0) (< dir 3)) -1)
-;;                                          ((and (>= dir 4) (< dir 7)) 1)
-;;                                          (t 0))
-;;                                    *height*)
-;;                                 *height*))
-;;    (decf (animal-energy animal))))
-;;
