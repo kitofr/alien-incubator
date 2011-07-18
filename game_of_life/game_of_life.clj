bd2e40512f11133eee35d0343a4a5eb092eaa6cf
kitofr <kitofr@gmail.com>
2011-07-18 21:14:35 +0200

    Renamed globals. Added move animal

--- game_of_life/game_of_life.clj
+++ game_of_life/game_of_life.clj
@@ -1,24 +1,23 @@
-(def *width* 100)
-(def *height* 30)
-(def *jungle* '(45 10 10 10))
-(def *plant-energy* 80)
-
-(def *plants* #{})
+(def width 100)
+(def height 30)
+(def jungle '(45 10 10 10))
+(def plant-energy 80)
+(def plants #{})
 
 (defn random-plant [left top width height]
   (let [pos (cons (+ left (rand-int width)) (list (+ top (rand-int height))))]
-        (conj *plants* (.hashCode pos))))
+        (conj plants (.hashCode pos))))
 
 (defn add-plants []
-  (apply #'random-plant *jungle*)
-  (random-plant 0 0 *width* *height*))
+  (apply #'random-plant jungle)
+  (random-plant 0 0 width height))
 
 (defstruct animal :x :y :energy :dir :genes)
 
-(def *animals*
+(def animals
   (list (struct-map animal 
-                    :x 50;;(/ 2 *width*)
-                    :y 15;;(/ 2 *height*)
+                    :x 50;;(/ 2 width)
+                    :y 15;;(/ 2 height)
                     :energy 1000
                     :dir 0
                     :genes (take 8
@@ -29,19 +28,22 @@
         x (:x animal)
         y (:y animal)]
     (assoc animal :x (mod (+ x
-                             (cond (and (>= dir 2) (< dir 5)
-                                    (or (= dir 1) (= dir 5)) 0)
-                                      (true -1))
-                                   *width*)
-                             *width*))
+                             (cond 
+                                (and (>= dir 2) (< dir 5)) 1
+                                (or (= dir 1) (= dir 5)) 0
+                                :else -1)
+                                   width)
+                             width))
     (assoc animal :y (mod (+ y
-                             (cond (and (>= dir 0) (< dir 3))
-                                   (or (= dir 
+                             (cond (and (>= dir 0) (< dir 3)) -1
+                                   (and (>= dir 4) (< dir 7)) 1
+                                   :else 0)
+                             height)
+                          height))
+    (dec (:energy animal))))
 
       
                                       
-
-
 ;;(defun move (animal)
 ;;  (let ((dir (animal-dir animal))
 ;;        (x (animal-x animal))
