74d0c10f0d64b9ef21ad7566ecf2a3713955cc73
Kristoffer Roupé <kitofr@gmail.com>
2011-11-20 20:59:37 +0100

    Almost done with winns?

--- pentago/pentago.clj
+++ pentago/pentago.clj
@@ -49,22 +49,6 @@
 
 ;(defn winner? [board] false)
 
-(defn play [board players]
-  (do
-    (print-board board)
-    (let [player (first players)]
-      (cond
-        (winner? board) (print (str (first (next players)) " Wins!\n"))
-        (full? board) (print "It's a Draw!\n")
-        true (do
-               (print (str "Select a square, " player ": "))
-               (flush)
-               (let [square (read)]
-                 (if (and square (available? square board))
-                   (recur (move player square board) (next players))
-                   (recur board players))))))))
-
-
 (defn get-row [index board]
   (nth board index))
 
@@ -81,26 +65,35 @@
 (defn get-indexes [sq board]
   (map #(get-row (first %1) (get-column (second %1) board)) sq))
 
-(get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] starting-board)
+(map five-in-a-row? 
+     (get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] starting-board))
+
 ;(let bind2nd f y = fun x -> f x y)
 (defn winner? [board]
-  (or (five-in-a-row? (map #(apply = (get-row %1 board)) indexes))
-      ;(five-in-a-row? (map #(apply = (get-column %1 board)) indexes))
-      ;(five-in-a-row? (get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] board))
-      ;(five-in-a-row? (get-indexes [[0 5] [1 4] [2 3] [3 2] [4 1] [5 0]] board))
+  (or (some #(= true %1) (map five-in-a-row? (map #(get-row %1 board) indexes)))
+      (some #(= true %1) (map five-in-a-row? (map #(get-column %1 board) indexes)))
+      (some #(= true %1) (map five-in-a-row? (get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] board)))
+      (some #(= true %1) (map five-in-a-row? (get-indexes [[0 5] [1 4] [2 3] [3 2] [4 1] [5 0]] board)))
       (apply = (get-indexes [[1 0] [2 1] [3 2] [4 3] [5 4]] board))
       (apply = (get-indexes [[0 1] [1 2] [2 3] [3 4] [4 5]] board))
       (apply = (get-indexes [[0 4] [1 3] [2 2] [3 1] [4 0]] board))
       (apply = (get-indexes [[1 5] [2 4] [3 3] [4 2] [5 1]] board))
       ))
 
+(defn play [board players]
+  (do
+    (print-board board)
+    (let [player (first players)]
+      (cond
+        (winner? board) (print (str (first (next players)) " Wins!\n"))
+        (full? board) (print "It's a Draw!\n")
+        true (do
+               (print (str "Select a square, " player ": "))
+               (flush)
+               (let [square (read)]
+                 (if (and square (available? square board))
+                   (recur (move player square board) (next players))
+                   (recur board players))))))))
 
-(winner? [[1 2 3 4 5 6]
-   [7 8 9 10 11 12]
-   [13 14 15 16 17 18]
-   [19 20 21 22 23 24]
-   [25 26 27 28 29 30]
-   [31 32 33 34 35 36]])
 
-(map five-in-a-row? (map #(get-row %1 starting-board) indexes))
-;(play starting-board players)
+(play starting-board players)
