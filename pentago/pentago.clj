590909225f22022aaa46f8f7eba02893a1be34e9
Kristoffer Roupé <kitofr@gmail.com>
2011-11-20 23:33:18 +0100

    winns

--- pentago/pentago.clj
+++ pentago/pentago.clj
@@ -72,20 +72,19 @@
 (defn winner? [board]
   (or (some #(= true %1) (map five-in-a-row? (map #(get-row %1 board) indexes)))
       (some #(= true %1) (map five-in-a-row? (map #(get-column %1 board) indexes)))
-      (some #(= true %1) (map five-in-a-row? (get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] board)))
-      (some #(= true %1) (map five-in-a-row? (get-indexes [[0 5] [1 4] [2 3] [3 2] [4 1] [5 0]] board)))
+      (five-in-a-row? (get-indexes [[0 0] [1 1] [2 2] [3 3] [4 4] [5 5]] board))
+      (five-in-a-row? (get-indexes [[0 5] [1 4] [2 3] [3 2] [4 1] [5 0]] board))
       (apply = (get-indexes [[1 0] [2 1] [3 2] [4 3] [5 4]] board))
       (apply = (get-indexes [[0 1] [1 2] [2 3] [3 4] [4 5]] board))
       (apply = (get-indexes [[0 4] [1 3] [2 2] [3 1] [4 0]] board))
-      (apply = (get-indexes [[1 5] [2 4] [3 3] [4 2] [5 1]] board))
-      ))
+      (apply = (get-indexes [[1 5] [2 4] [3 3] [4 2] [5 1]] board))))
 
 (defn play [board players]
   (do
     (print-board board)
     (let [player (first players)]
       (cond
-        (winner? board) (print (str (first (next players)) " Wins!\n"))
+        (winner? board) (print (str (first "*** Player: " (next players)) " Wins! ***\n"))
         (full? board) (print "It's a Draw!\n")
         true (do
                (print (str "Select a square, " player ": "))
